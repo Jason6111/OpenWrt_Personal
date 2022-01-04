788a63cbbe31b98c6e0a8bade258e3e7b8404d2d
小尨 <43496459+Jason6111@users.noreply.github.com>
2022-01-04 09:57:24 +0800

    Update diy-part_X861.sh

--- diy_script/diy-part_X861.sh
+++ diy_script/diy-part_X861.sh
@@ -12,6 +12,9 @@
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
+#LINUX_VERSION
+sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(LINUX_VERSION)-/g' include/image.mk
+
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
