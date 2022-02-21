dc02123c8bb9b29e4769407beab610678fc39ccf
小尨 <43496459+Jason6111@users.noreply.github.com>
2022-02-21 09:35:58 +0800

    Update diy-part_R4S2.sh

--- diy_script/diy-part_R4S2.sh
+++ diy_script/diy-part_R4S2.sh
@@ -26,4 +26,4 @@ sed -i 's/R.*.*.2/& Compiled by Jason/' package/lean/default-settings/files/zzz-
 # 添加新主题  
 rm -rf ./feeds/luci/themes/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git ./feeds/luci/themes/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-app-argon-config.git ./feeds/luci/themes/luci-app-argon-config
+git clone https://github.com/jerrykuku/luci-app-argon-config.git ./package/lean/luci-app-argon-config
