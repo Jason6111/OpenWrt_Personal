fa834764d437982dfe26edc514a079f6c92876aa
小尨 <43496459+Jason6111@users.noreply.github.com>
2022-06-08 08:22:35 +0800

    Update diy-part_X86_5152.sh

--- diy_script/diy-part_X86_5152.sh
+++ diy_script/diy-part_X86_5152.sh
@@ -17,8 +17,6 @@
 #rm -rf package/utils/ucode/
 #svn co https://github.com/openwrt/openwrt/trunk/package/utils/ucode package/utils/ucode
 
-sed -i 's/luci-lib-ipkg/luci-base/g' package/luci-app-ssr-plus/Makefile
-
 #日期
 sed -i 's/os.date(/&"%Y-%m-%d %H:%M:%S"/' package/lean/autocore/files/x86/index.htm
 
