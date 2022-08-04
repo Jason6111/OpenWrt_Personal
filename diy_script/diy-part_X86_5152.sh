29ebd750bded33fbf5cd907d6d0c3b3333aed98d
小尨 <43496459+Jason6111@users.noreply.github.com>
2022-08-04 21:02:02 +0800

    Update diy-part_X86_5152.sh

--- diy_script/diy-part_X86_5152.sh
+++ diy_script/diy-part_X86_5152.sh
@@ -53,5 +53,5 @@ sed -i 's/echo $h/echo $g/g' package/lean/autocore/files/x86/autocore
 #sed -i 's/PKG_HASH.*/PKG_HASH:=skip/' feeds/packages/utils/containerd/Makefile
 #tr
 #sed -i "s/&& LINUX_VERSION_CODE < KERNEL_VERSION(5, 18, 0)//g" package/lean/shortcut-fe/fast-classifier/src/fast-classifier.c
-#sed -i s/5.18/5.19/g  target/linux/x86/Makefile
-#sed -i s/5.15/5.19/g  target/linux/x86/Makefile
+sed -i s/5.15/5.19/g  target/linux/x86/Makefile
+sed -i s/5.18/5.19/g  target/linux/x86/Makefile
