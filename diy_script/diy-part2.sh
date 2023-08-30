791999a3be611d276e136ecc362ed4c5da1f6ba0
小尨 <43496459+Jason6111@users.noreply.github.com>
2023-08-30 09:12:53 +0800

    Update diy-part2.sh

--- diy_script/diy-part2.sh
+++ diy_script/diy-part2.sh
@@ -50,9 +50,9 @@ sed -i '/h=${g}.*/d' package/lean/autocore/files/x86/autocore
 sed -i 's/echo $h/echo $g/g' package/lean/autocore/files/x86/autocore
 
 # 临时
-sed -i s/5.15/6.1/g  target/linux/x86/Makefile
-#sed -i "s/73a2498dc95934c225d358707e7f7d060b5ce81aa45260ada09cbd15207d27d1/5279eb1cb7555cf9292423cc9f672dc43e6e214b3411a6df26a6a1cfa59d88b7/g" ./feeds/helloworld/ipt2socks/Makefile
-curl -o ./feeds/packages/multimedia/aliyundrive-webdav/Makefile https://raw.githubusercontent.com/Jason6111/OpenWrt_Personal/main/other/aliyun/Makefile
+sed -i 's/5.15/6.1/g'  target/linux/x86/Makefile
+#sed -i 's/73a2498dc95934c225d358707e7f7d060b5ce81aa45260ada09cbd15207d27d1/5279eb1cb7555cf9292423cc9f672dc43e6e214b3411a6df26a6a1cfa59d88b7/g' ./feeds/helloworld/ipt2socks/Makefile
+curl -o ./feeds/packages/multimedia/aliyundrive-webdav/Makefile https://raw.githubusercontent.com/coolsnowwolf/packages/aea60b5432fad984c0a4013bad0f0c5e00dcd115/multimedia/aliyundrive-webdav/Makefile
 
 # zh netdata
 rm -rf ./feeds/luci/applications/luci-app-netdata/
