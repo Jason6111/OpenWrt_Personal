1a52918679080ef4aba24d958b0f1ba755478e4d
小尨 <43496459+Jason6111@users.noreply.github.com>
2023-09-03 11:10:11 +0800

    Update diy-part2.sh

--- diy_script/diy-part2.sh
+++ diy_script/diy-part2.sh
@@ -54,6 +54,8 @@ sed -i 's/5.15/6.1/g'  target/linux/x86/Makefile
 #curl -o ./feeds/packages/multimedia/aliyundrive-webdav/Makefile https://raw.githubusercontent.com/coolsnowwolf/packages/aea60b5432fad984c0a4013bad0f0c5e00dcd115/multimedia/aliyundrive-webdav/Makefile
 sed -i 's/1.68.1/1.72.0/g' feetd/packages/lang/rust
 /Makefile
+sed -i 's/ccb051df5701d4c588e3d0558f83e73e7eea0a9b165dab3e39dd2db8a6a25d03/ea9d61bbb51d76b6ea681156f69f0e0596b59722f04414b01c6e100b4b5be3a1/g' feetd/packages/lang/rust
+/Makefile
 
 # zh netdata
 rm -rf ./feeds/luci/applications/luci-app-netdata/
