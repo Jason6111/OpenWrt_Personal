5c9035bb58e27f7757429f596f374abba66f245b
小尨 <43496459+Jason6111@users.noreply.github.com>
2023-11-02 02:08:18 -0700

    Update diy-part2.sh

--- diy_script/diy-part2.sh
+++ diy_script/diy-part2.sh
@@ -43,5 +43,5 @@ sed -i 's/5.15/6.1/g'  target/linux/x86/Makefile
 # zh netdata
 rm -rf ./feeds/luci/applications/luci-app-netdata/
 git clone https://github.com/Jason6111/luci-app-netdata ./feeds/luci/applications/luci-app-netdata/
+# 开启netdata温控监测
 sed -i 's/charts\.d = no/charts\.d = yes/g' ./feeds/luci/applications/luci-app-netdata/root/etc/netdata/netdata.conf
-sed -i 's/netdata\/netdata/Jason6111\/netdata/g' ./feeds/packages/admin/netdata/Makefile
