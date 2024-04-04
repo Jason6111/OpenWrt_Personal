5928ce37e5e80312f20d50360424125a0fdbe355
小尨 <43496459+Jason6111@users.noreply.github.com>
2024-04-04 22:09:40 +0800

    Update diy-part2.sh

--- diy_script/diy-part2.sh
+++ diy_script/diy-part2.sh
@@ -11,34 +11,34 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.31.66/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.31.66/g' ./package/base-files/files/bin/config_generate
 
 # 日期
-sed -i 's/os.date(/&"%Y-%m-%d %H:%M:%S"/' package/lean/autocore/files/x86/index.htm
+sed -i 's/os.date(/&"%Y-%m-%d %H:%M:%S"/' ./package/lean/autocore/files/x86/index.htm
 
 # 关闭串口跑码
-sed -i 's/console=tty0//g'  target/linux/x86/image/Makefile
-sed -i 's/%V, %C/[Year] | by Jason /g' package/base-files/files/etc/banner
-sed -i "s/Year/$(TZ=':Asia/Shanghai' date '+%Y')/g" package/base-files/files/etc/banner
-sed -i '/logins./a\                                          by Jason' package/base-files/files/etc/profile
+sed -i 's/console=tty0//g'  ./target/linux/x86/image/Makefile
+sed -i 's/%V, %C/[Year] | by Jason /g' ./package/base-files/files/etc/banner
+sed -i "s/Year/$(TZ=':Asia/Shanghai' date '+%Y')/g" ./package/base-files/files/etc/banner
+sed -i '/logins./a\                                          by Jason' ./package/base-files/files/etc/profile
 
 # Modify default passwd
-sed -i '/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF./ d' package/lean/default-settings/files/zzz-default-settings
+sed -i '/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF./ d' ./package/lean/default-settings/files/zzz-default-settings
 
 # ID
-sed -i "s/DISTRIB_REVISION='R.*.*.[0-9]/& Compiled by Jason/" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/DISTRIB_REVISION='R.*.*.[0-9]/& Compiled by Jason/" ./package/lean/default-settings/files/zzz-default-settings
 
 # 主题背景
 mkdir -p ./feeds/luci/themes/luci-theme-argon-mod/htdocs/luci-static/argon/background/ && curl -o ./feeds/luci/themes/luci-theme-argon-mod/htdocs/luci-static/argon/background/Network.mp4 https://raw.githubusercontent.com/Jason6111/OpenWrt_Personal/main/other/argon/video/default/Network.mp4
 
 # 恢复主机型号
-sed -i 's/(dmesg | grep .*/{a}${b}${c}${d}${e}${f}/g' package/lean/autocore/files/x86/autocore
+sed -i 's/(dmesg | grep .*/{a}${b}${c}${d}${e}${f}/g' ./package/lean/autocore/files/x86/autocore
 #sed -i 's/(cat \/tmp.*/\{a}${b}${c}${d}${e}${f}/g' package/lean/autocore/files/x86/autocore
-sed -i '/h=${g}.*/d' package/lean/autocore/files/x86/autocore
-sed -i 's/echo $h/echo $g/g' package/lean/autocore/files/x86/autocore
+sed -i '/h=${g}.*/d' ./package/lean/autocore/files/x86/autocore
+sed -i 's/echo $h/echo $g/g' ./package/lean/autocore/files/x86/autocore
 
 # 临时
-sed -i 's/6.1/6.6/g'  target/linux/x86/Makefile
+sed -i 's/6.1/6.6/g'  ./target/linux/x86/Makefile
 
 # zh netdata
 rm -rf ./feeds/luci/applications/luci-app-netdata/
@@ -46,7 +46,3 @@ git clone https://github.com/Jason6111/luci-app-netdata ./feeds/luci/application
 
 # 开启netdata温控监测
 sed -i 's/charts\.d = no/charts\.d = yes/g' ./feeds/luci/applications/luci-app-netdata/root/etc/netdata/netdata.conf
-
-#刷新固件docker无法启用
-sed -i '/exit 0/i \/etc\/init.d\/docker restart' package/base-files/files/etc/rc.local
-
