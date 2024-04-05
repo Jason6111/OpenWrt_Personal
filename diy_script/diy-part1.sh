8fd99e7379ee7eb82613cc25a97e791ffe37bce9
小尨 <43496459+Jason6111@users.noreply.github.com>
2024-04-06 04:46:53 +0800

    Update diy-part1.sh

--- diy_script/diy-part1.sh
+++ diy_script/diy-part1.sh
@@ -20,6 +20,3 @@ echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.defau
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 #echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
-
-#刷新固件docker无法启用
-sed -i '/exit 0/i \/etc\/init.d\/docker restart' ./package/base-files/files/etc/rc.local
